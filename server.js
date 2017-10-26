76bc40db02bd27c2be3d38412d7216ae60d338d7
Rob Warren <robw219@gmail.com>
2017-10-26 14:09:07 -0600

    Added awesome styling

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var methodOverride = require("method-override");
 var exphbs = require("express-handlebars");
 
 var app = express();
-app.use(express.static(__dirname = "/public"));
+app.use(express.static(__dirname = "public"));
 
 app.use(bodyParser.urlencoded({
 	extended: false
@@ -19,6 +19,6 @@ app.set("view engine", "handlebars");
 var routes = require("./controllers/burgers_controllers.js");
 app.use("/", routes);
 
-var port = 3000;
+var port = process.env.PORT || 3000;
 app.listen(port);
 
