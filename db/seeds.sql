4ad1eb3cef052327525d806fd76aa45248a6e705
Rob Warren <robw219@gmail.com>
2017-10-26 11:06:31 -0600

    working but stil need style

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("BBQ Bacon Burger", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Double Patty Burger", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Buffalo Burger", FALSE);
